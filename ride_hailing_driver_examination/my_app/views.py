d3275900c0e3afdb4911334bc770fb32b9c59322
hekaiyou <hekaiyou@qq.com>
2025-09-23 17:00:34 +0800

    模版-登陆

--- ride_hailing_driver_examination/my_app/views.py
+++ ride_hailing_driver_examination/my_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        id_card = request.POST['id_card']
+        password = request.POST['password']
+        # 判断用户名和密码
+        user = authenticate(request, username=id_card, password=password)
+        if user is not None:
+            login(request, user)
+            # 登录成功后重定向至主页
+            return redirect('home')
+        else:
+            messages.error(request, '身份证号或密码错误')
+    # 渲染登录页面
+    return render(request, 'login.html')
--- ride_hailing_driver_examination/ride_hailing_driver_examination/urls.py
+++ ride_hailing_driver_examination/ride_hailing_driver_examination/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from my_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # 登录视图
+    path('login/', views.login_view, name='login'),
 ]
