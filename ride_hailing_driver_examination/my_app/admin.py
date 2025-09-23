0e708b7f039531c5fd63dac87a9439c7cd95b89f
hekaiyou <hekaiyou@qq.com>
2025-09-23 18:16:25 +0800

    模型Student

--- ride_hailing_driver_examination/my_app/admin.py
+++ ride_hailing_driver_examination/my_app/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from .models import Student
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id_card', 'profession_type',)
+    search_fields = ('id_card',)
--- ride_hailing_driver_examination/my_app/models.py
+++ ride_hailing_driver_examination/my_app/models.py
@@ -1,1 +1,17 @@
 from django.db import models
+
+class Student(models.Model):
+
+    class ProfessionType(models.TextChoices):
+        RIDER = 'Rider', '网络预约出租汽车驾驶员'
+        OTHER = 'Other', '其他'
+
+    id_card = models.CharField(max_length=18, unique=True, verbose_name='身份证号')
+    profession_type = models.CharField(max_length=100, choices=ProfessionType.choices, default=ProfessionType.RIDER, verbose_name='从业类型')
+
+    def __str__(self):
+        return self.id_card
+
+    class Meta:
+        verbose_name = "学员"
+        verbose_name_plural = "学员"
