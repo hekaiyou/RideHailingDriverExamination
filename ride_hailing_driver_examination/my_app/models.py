18ee47149d3314fcac9349b6ad816e91f0742c01
hekaiyou <hekaiyou@qq.com>
2025-09-23 16:12:18 +0800

    用户模型-从业类型

--- ride_hailing_driver_examination/my_app/models.py
+++ ride_hailing_driver_examination/my_app/models.py
@@ -2,7 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
+    # 用户从业类型的枚举
+    class OccupationType(models.TextChoices):
+        RIDER = 'Rider', '网络预约出租汽车驾驶员'
+        OTHER = 'Other', '其他'
+
     id_card = models.CharField(max_length=18, unique=True)
+    occupation_type = models.CharField(
+        max_length=30,
+        choices=OccupationType.choices,
+        default=OccupationType.RIDER,
+    )
 
     def __str__(self):
         return self.username
